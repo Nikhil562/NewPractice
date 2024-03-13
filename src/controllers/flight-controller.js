7093ffc73510b6bfed4a70a493589daee85e8573
bhaiyu <niksharma34@gmail.com>
2024-03-13 21:41:39 +0530

    Added Flights search crud with few filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,5 +1,4 @@
 const {FlightService} = require('../services/index');
-// const { SuccessCodes } = require('../utils/error-codes');
 
 const flightService = new FlightService();
 
@@ -13,7 +12,6 @@ const create = async (req, res) => {
             message: 'Successfully created a flight'
         })
     } catch (error) {
-        console.log("citycontorller1"),
         console.log(error);
         return res.status(500).json({
             data: {},
@@ -24,6 +22,28 @@ const create = async (req, res) => {
     }
 }
 
+const getAll = async (req, res) => {
+    try {
+        console.log(req.query);
+        const response = await flightService.getAllFlightData(req.query);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully fetched the flights'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the flights',
+            err: error
+        });
+    }
+}
+
 module.exports = {
-    create
+    create,
+    getAll
 }
